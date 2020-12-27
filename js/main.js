efcbeffa5206ee1f2607ebe5314cb8996cad44ff
Mohit Dodhia <mohit.daddyscode@gmail.com>
2020-12-27 16:36:46 +0530

    Added files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+Author       : Mohit Dodhia
+ */
+
+$(document).ready(function () {
+    // Tooltip
+    if ($('[data-toggle="tooltip"]').length > 0) {
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+});
