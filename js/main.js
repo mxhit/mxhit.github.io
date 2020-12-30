e82c13f66a67563714e12ef73663a758c29540ef
Mohit Dodhia <mohit.daddyscode@gmail.com>
2020-12-30 21:13:46 +0530

    Added dark mode toggle

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $(document).ready(function () {
     if ($('[data-toggle="tooltip"]').length > 0) {
         $('[data-toggle="tooltip"]').tooltip();
     }
+
+    document.getElementById("dark-mode-switch").checked = false;
 });
+
+function darkModeToggle() {
+    var element = document.body;
+    var switchLabel = document.getElementById("switch");
+
+    element.classList.toggle("dark-mode");
+
+    var darkModeSwitch = document.getElementById("dark-mode-switch");
+    if (darkModeSwitch.checked) {
+        switchLabel.textContent = "🌙";
+    } else {
+        switchLabel.textContent = "☀️";
+    }
+}
